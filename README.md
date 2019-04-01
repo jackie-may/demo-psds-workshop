211d505b5ea44b035a3f7cbba55251d50737e747
jaketrent <trent.jake@gmail.com>
2019-04-01 15:30:23 -0600

    add text

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 - [AsideLayout](https://design-system.pluralsight.com/components/layout/#aside-layout)
 - [Avatar](https://design-system.pluralsight.com/components/avatar/)
 - [Core spacing](https://design-system.pluralsight.com/core/spacing/)
+- [Text](https://design-system.pluralsight.com/components/text/)
+- [Core typography](https://design-system.pluralsight.com/core/typography/)
--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import { AsideLayout } from '@pluralsight/ps-design-system-layout/react'
 import Avatar from '@pluralsight/ps-design-system-avatar/react'
+import { Heading } from '@pluralsight/ps-design-system-text/react'
 import React from 'react'
 import styleable from 'react-styleable'
 
@@ -16,6 +17,9 @@ export default styleable(css)(function App(props) {
               size={Avatar.sizes.xLarge}
               src="https://gravatar.com/avatar/63a1fa126f541c0f0ecf1d74f7a40640?s=320&r=g&d=https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fps-cdn%2Fdesign-system%2Fassets%2Ftransparent.gif"
             />
+            <Heading className={props.css.authorName}>
+              <h1>Jake Trent</h1>
+            </Heading>
           </AsideLayout.Aside>
         }
         main={<AsideLayout.Main>qwer</AsideLayout.Main>}
