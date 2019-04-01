428f67b1030c23692820976d2f869b8aa8156302
jaketrent <trent.jake@gmail.com>
2019-04-01 15:21:49 -0600

    add avatar

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 - [Prism](https://bit.ly/prismnav)
 - [AsideLayout](https://design-system.pluralsight.com/components/layout/#aside-layout)
+- [Avatar](https://design-system.pluralsight.com/components/avatar/)
+- [Core spacing](https://design-system.pluralsight.com/core/spacing/)
--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 import { AsideLayout } from '@pluralsight/ps-design-system-layout/react'
+import Avatar from '@pluralsight/ps-design-system-avatar/react'
 import React from 'react'
 import styleable from 'react-styleable'
 
 import css from './app.module.css'
 
-export default styleable(css)(function App() {
+export default styleable(css)(function App(props) {
   return (
     <div className="app">
       <AsideLayout
-        aside={<AsideLayout.Aside>asdf</AsideLayout.Aside>}
+        aside={
+          <AsideLayout.Aside className={props.css.aside}>
+            <Avatar
+              name="Jake Trent"
+              size={Avatar.sizes.xLarge}
+              src="https://gravatar.com/avatar/63a1fa126f541c0f0ecf1d74f7a40640?s=320&r=g&d=https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fps-cdn%2Fdesign-system%2Fassets%2Ftransparent.gif"
+            />
+          </AsideLayout.Aside>
+        }
         main={<AsideLayout.Main>qwer</AsideLayout.Main>}
       />
     </div>
